b13e82044d984fc93c77e2e38d29533a5d657848
Pierre Philipin <Pierre.Philipin@faymonville.com>
2025-05-22 09:54:17 +0200

    Interface for common custom DataGridColumn properties

---
+++ FilterDataGrid.Net/IDataGridColumn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterDataGrid
+{
+    /// <summary>
+    /// Represents a column in a data grid with filtering capabilities.
+    /// </summary>
+    public interface IDataGridColumn
+    {
+        /// <summary>
+        /// Gets or sets the field name associated with the column.
+        /// </summary>
+        string FieldName { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the column is currently filtered.
+        /// </summary>
+        bool IsColumnFiltered { get; set; }
+    }
+}
